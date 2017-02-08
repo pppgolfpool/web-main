2bbbd9f2de9a85ab06c0268595e012e4ee3bea70
Michael Daw <michael.k.daw@gmail.com>
2017-02-08 16:33:36 -0700

    adding search feature to picks summary

--- src/microservices/picks/pick-summary.ts
+++ src/microservices/picks/pick-summary.ts
@@ -1,5 +1,6 @@
 import { inject } from 'aurelia-framework';
 import { PickClient } from './pickClient';
+import { observable } from 'aurelia-framework';
 
 @inject(PickClient)
 export class PickSummary {
@@ -9,10 +10,27 @@ export class PickSummary {
 
   private readonly pickClient: PickClient;
   private picks: Object;
+  private subPicks: Object;
+  @observable search: string;
 
-  async attached(){
+  async attached() {
     this.picks = await this.pickClient.getPickSummary();
+    this.subPicks = this.picks;
   }
 
+  searchChanged() {
+    if(this.search === ''){
+      this.subPicks = this.picks;
+      return;
+    }
+    this.subPicks = {};
+    let names = Object.getOwnPropertyNames(this.picks);
+    for(let rawName of names){
+      let name = rawName.substr(0, rawName.indexOf(':'))
+      if(name.toLowerCase().includes(this.search.toLowerCase())){
+        this.subPicks[rawName] = this.picks[rawName];
+      }
+    }
+  }
 }
 
