ad6a02faf474f1728329be4bebdc0b249f8140a1
Michael Daw <michael.k.daw@gmail.com>
2017-02-08 17:02:24 -0700

    added golfer search to the picks summary

--- src/microservices/picks/pick-summary.ts
+++ src/microservices/picks/pick-summary.ts
@@ -10,27 +10,52 @@ export class PickSummary {
 
   private readonly pickClient: PickClient;
   private picks: Object;
-  private subPicks: Object;
-  @observable search: string;
+  private pooliePicks: Object;
+  private golferPicks: Object;
+  @observable search: string = '';
+  @observable golfer: string = '';
 
   async attached() {
     this.picks = await this.pickClient.getPickSummary();
-    this.subPicks = this.picks;
+    this.pooliePicks = this.picks;
+    this.golferPicks = this.pooliePicks;
+    console.log(this.picks);
   }
 
   searchChanged() {
-    if(this.search === ''){
-      this.subPicks = this.picks;
+    if (this.search === '') {
+      this.pooliePicks = this.picks;
+      this.golferPicks = this.pooliePicks;
+    } else {
+      this.pooliePicks = {};
+      let names = Object.getOwnPropertyNames(this.picks);
+      for (let rawName of names) {
+        let name = rawName.substr(0, rawName.indexOf(':'))
+        if (name.toLowerCase().includes(this.search.toLowerCase())) {
+          this.pooliePicks[rawName] = this.picks[rawName];
+        }
+      }
+      this.golferPicks = this.pooliePicks;
+    }
+    
+    if (this.golfer === '') {
       return;
     }
-    this.subPicks = {};
-    let names = Object.getOwnPropertyNames(this.picks);
-    for(let rawName of names){
-      let name = rawName.substr(0, rawName.indexOf(':'))
-      if(name.toLowerCase().includes(this.search.toLowerCase())){
-        this.subPicks[rawName] = this.picks[rawName];
+    this.golferPicks = {};
+    for (let property in this.pooliePicks) {
+      if (this.pooliePicks.hasOwnProperty(property)) {
+        let golfers = this.pooliePicks[property] as Array<string>;
+        for (let player of golfers) {
+          if (player["PlayerName"].toLowerCase().includes(this.golfer.toLowerCase())) {
+            this.golferPicks[property] = this.pooliePicks[property];
+          }
+        }
       }
     }
+  }
+
+  golferChanged() {
+    this.searchChanged();
   }
 }
 
